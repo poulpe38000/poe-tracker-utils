3408de71807722b44d597135d3d2e03a567cd9dd
ZMSK7608 <julien.minella.ext@orange.com>
2019-05-28 11:27:31 +0200

    Updates

---
+++ precache-manifest.591293da021bd444b160d4c8fbdb624c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20996fcfc5cf79bd0d1ef002a9aafdf2",
+    "url": "/poetracker/index.html"
+  },
+  {
+    "revision": "1ea12f29a4fed8b7d29b",
+    "url": "/poetracker/static/css/main.d2718870.chunk.css"
+  },
+  {
+    "revision": "df0d1c3ce68c614ab60e",
+    "url": "/poetracker/static/js/2.e229d57e.chunk.js"
+  },
+  {
+    "revision": "1ea12f29a4fed8b7d29b",
+    "url": "/poetracker/static/js/main.a1c41b41.chunk.js"
+  },
+  {
+    "revision": "f86ffec9f2f57b0fa5d8",
+    "url": "/poetracker/static/js/runtime~main.f2b7c437.js"
+  }
+]);
